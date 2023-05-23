0c691d701ae9b9cd529517e8ca7f8cd6fc8b0d16
Neosoft <Neosoft@DESKTOP-AAIK2PJ>
2023-05-23 16:26:11 +0530

    Redis Cache

--- RedisCache/src/main/java/com/javatechie/redis/RedisCacheApplication.java
+++ RedisCache/src/main/java/com/javatechie/redis/RedisCacheApplication.java
@@ -1,16 +1,10 @@
 package com.javatechie.redis;
 
-import com.javatechie.redis.entity.Product;
-import com.javatechie.redis.respository.ProductDao;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @SpringBootApplication
 @EnableCaching
